3647f8e16706ce534f4bca47329a4cb3fd11a009
yesid10 <yesidvanegas68@gmail.com>
2025-06-10 12:29:31 -0500

    Instalando CORS y conexion realizada con el front

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const connectDB = require('./db');
 const productosRouter = require('./routes/productos');
 const preciosEspecialesRouter = require('./routes/preciosEspeciales');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
